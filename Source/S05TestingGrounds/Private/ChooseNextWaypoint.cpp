ac1b4cdc547209d898a39ebc103d78f085c4079d
Paul Buschmann <paul.buschmann@chello.at>
2018-04-30 23:42:51 +0200

    Create PatrolRoute
    component for AI to choose patrol waypoints

--- Source/S05TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/S05TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,17 +2,27 @@
 
 #include "ChooseNextWaypoint.h"
 #include "Runtime/AIModule/Classes/BehaviorTree/BlackboardComponent.h"
-
-
-
+#include "PatrolRoute.h"
+#include "AIController.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// get patrol points
+	UPatrolRoute* PatrolRoute = OwnerComp.GetAIOwner()->GetPawn()->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	TArray<AActor*> PatrolPoints = PatrolRoute->GetPatrolPoints();
+
+	// set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
-
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	if (PatrolPoints.Num() == 0) { return EBTNodeResult::Failed; }
+	BlackboardComp->SetValueAsObject(NextWaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+	// cycle index
+	Index++;
+	Index %= PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
 
 	return EBTNodeResult::Succeeded;
 }
