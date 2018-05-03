c8a2ff5c0df77316ac8eabc31d4f8e5ff47596cc
BushuoPaul Buschmann <paul.buschmann@chello.at>
2018-05-03 15:24:57 +0200

    Update @AIBehaviour
    AI now patrols, chases and remains suspicous for a short amount of time

--- Source/S05TestingGrounds/S05TestingGroundsGameMode.cpp
+++ Source/S05TestingGrounds/S05TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AS05TestingGroundsGameMode::AS05TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
